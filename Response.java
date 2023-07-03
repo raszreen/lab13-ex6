89b3123a1b3debdcd1336b4869826f393cbdeb12
raszreen <130258260+raszreen@users.noreply.github.com>
2023-07-04 00:14:51 +0800

    Create Response.java

---
+++ Response.java
@@ -0,0 +1,10 @@
+public class Response {
+    private String translatedText;
+    public Response(String translatedText) {
+        this.translatedText = translatedText;
+    }
+    public String getTranslatedText() {
+        return translatedText;
+    }
+}
+   //I understand that you want me to translate a text from English to Spanish. I can do that for you. Please provide me with the text that you want me to translate.
